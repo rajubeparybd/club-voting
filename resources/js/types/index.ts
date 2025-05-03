780ee2091df1b7bc00c3cc837cac33a536464378
Raju Bepary <raju174829@gmail.com>
2025-05-03 22:57:52 +0600

    refactor: update ClubsIndex to utilize usePage hook and reorganize type definitions

--- resources/js/types/index.ts
+++ resources/js/types/index.ts
@@ -1,3 +1,6 @@
+import { LucideIcon } from 'lucide-react';
+import type { Config } from 'ziggy-js';
+
 export interface BreadcrumbItem {
     title: string;
     href: string;
@@ -45,4 +48,47 @@ export interface PaginatedData<T> {
     prev_page_url: string | null;
     to: number;
     total: number;
+}
+
+export interface Auth {
+    user: User;
+    roles?: string[];
+    permissions?: string[];
+}
+
+export interface BreadcrumbItem {
+    title: string;
+    href: string;
+}
+
+export interface NavGroup {
+    title: string;
+    items: NavItem[];
+}
+
+export interface NavItem {
+    title: string;
+    href: string;
+    icon?: LucideIcon | null;
+    isActive?: boolean;
+}
+
+export interface SharedData {
+    name: string;
+    quote: { message: string; author: string };
+    auth: Auth;
+    ziggy: Config & { location: string };
+    sidebarOpen: boolean;
+    [key: string]: unknown;
+}
+
+export interface User {
+    id: number;
+    name: string;
+    email: string;
+    avatar?: string;
+    email_verified_at: string | null;
+    created_at: string;
+    updated_at: string;
+    [key: string]: unknown;
 }
