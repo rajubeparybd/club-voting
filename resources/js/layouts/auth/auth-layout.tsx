526efb485b1b94e4a2b3f637c1c9e64308bb7445
Raju Bepary <raju174829@gmail.com>
2025-05-07 18:43:23 +0600

    feat: add success messages for password update and logout actions

--- resources/js/layouts/auth/auth-layout.tsx
+++ resources/js/layouts/auth/auth-layout.tsx
@@ -1,7 +1,9 @@
 import { Toaster } from '@/components/ui/sonner';
+import useFlashNotifications from '@/hooks/use-flash-notifications';
 import AuthLayoutTemplate from '@/layouts/auth/auth-card-layout';
 
 export default function AuthLayout({ children, title, description, ...props }: { children: React.ReactNode; title: string; description: string }) {
+    useFlashNotifications();
     return (
         <AuthLayoutTemplate title={title} description={description} {...props}>
             {children}
