a932eaee04148cc7a80ad8c7af575732d0d0c712
Raju Bepary <raju174829@gmail.com>
2025-05-19 23:00:04 +0600

    feat: update AppLogoIcon and AppLogo components for enhanced design and dynamic name display

--- resources/js/components/app/app-logo.tsx
+++ resources/js/components/app/app-logo.tsx
@@ -1,13 +1,15 @@
+import { usePage } from '@inertiajs/react';
 import AppLogoIcon from './app-logo-icon';
 
 export default function AppLogo() {
+    const name = usePage().props.name;
     return (
         <>
             <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-md">
-                <AppLogoIcon className="size-5 fill-current text-white dark:text-black" />
+                <AppLogoIcon className="size-10 fill-current text-white dark:text-black" />
             </div>
             <div className="ml-1 grid flex-1 text-left text-sm">
-                <span className="mb-0.5 truncate leading-none font-semibold">Laravel Starter Kit</span>
+                <span className="mb-0.5 truncate leading-none font-semibold">{(name as string) || 'BUBT Club'}</span>
             </div>
         </>
     );
